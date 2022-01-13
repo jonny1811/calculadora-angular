84011c745aa38045ff6bf17320a35997e41e9fa5
jonny1811 <jonny@quanto.la>
2022-01-13 17:42:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+	title = 'Aplicación Calculadora';
+	operatorA: number = 0;
+	operatorB: number = 0;
+	result: number = 0;
+
+	add(): void {
+		this.result = this.operatorA + this.operatorB;
+		console.log(this.result)
+	}
 }
